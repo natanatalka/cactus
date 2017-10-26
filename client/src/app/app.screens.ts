df61403abf51ef33f6599888cdf4bda832df21af
PowWow Admin <admin@studio.dev.powwowinc.net>
2017-10-26 16:56:39 +0300

    [Studio] Delete screen "Screen"

--- client/src/app/app.screens.ts
+++ client/src/app/app.screens.ts
@@ -1,15 +1,9 @@
 /***  Generated file, do not change.  */
 import { Page } from 'ionic-angular/navigation/nav-util';
 import { MenuComponent } from './menu/menu.component';
-import { Screen_PhonePortrait } from '../pages/Screen/PhonePortrait/Screen';
 export class Screens {
   static declarations: Page[] = [
-    MenuComponent,
-    Screen_PhonePortrait
+    MenuComponent
   ];
-  static mapping = {
-    'Screen': {
-      PhonePortrait: Screen_PhonePortrait
-    }
-  }
+  static mapping = {}
 }
--- client/src/pages/Screen/PhonePortrait/Screen.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-import { Screen } from 'app/screen';
-
-@Component({
-  selector: 'screen-screen-phoneportrait',
-  templateUrl: 'Screen.html'
-})
-export class Screen_PhonePortrait extends Screen {
-}
