1a5cbe2e1331cd413cad9309c301baaac5d4224b
test <test@test.com>
2017-10-26 17:57:10 +0300

    [Studio] Delete screen "Blank"

--- client/src/app/app.screens.ts
+++ client/src/app/app.screens.ts
@@ -2,21 +2,16 @@
 import { Page } from 'ionic-angular/navigation/nav-util';
 import { MenuComponent } from './menu/menu.component';
 import { Screen_PhonePortrait } from '../pages/Screen/PhonePortrait/Screen';
-import { Blank_PhonePortrait } from '../pages/Blank/PhonePortrait/Blank';
 import { 1 _PhonePortrait } from '../pages/1/PhonePortrait/1';
 export class Screens {
   static declarations: Page[] = [
     MenuComponent,
     Screen_PhonePortrait,
-    Blank_PhonePortrait,
     1 _PhonePortrait
   ];
   static mapping = {
     'Screen': {
       PhonePortrait: Screen_PhonePortrait
-    },
-    'Blank': {
-      PhonePortrait: Blank_PhonePortrait
     },
     '1': {
       PhonePortrait: 1 _PhonePortrait
--- client/src/mocks/index.ts
+++ client/src/mocks/index.ts
@@ -1,3 +1,2 @@
 export * from './1/index';
-export * from './Blank/index';
 export * from './Screen/index';
