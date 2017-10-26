2275bf6048abd443ea930510dcaa2385259384a4
test <test@test.com>
2017-10-26 17:56:54 +0300

    [Studio] Delete screen "Monday"

--- client/src/app/app.screens.ts
+++ client/src/app/app.screens.ts
@@ -3,14 +3,12 @@ import { Page } from 'ionic-angular/navigation/nav-util';
 import { MenuComponent } from './menu/menu.component';
 import { Screen_PhonePortrait } from '../pages/Screen/PhonePortrait/Screen';
 import { Blank_PhonePortrait } from '../pages/Blank/PhonePortrait/Blank';
-import { Monday_PhonePortrait } from '../pages/Monday/PhonePortrait/Monday';
 import { 1 _PhonePortrait } from '../pages/1/PhonePortrait/1';
 export class Screens {
   static declarations: Page[] = [
     MenuComponent,
     Screen_PhonePortrait,
     Blank_PhonePortrait,
-    Monday_PhonePortrait,
     1 _PhonePortrait
   ];
   static mapping = {
@@ -19,9 +17,6 @@ export class Screens {
     },
     'Blank': {
       PhonePortrait: Blank_PhonePortrait
-    },
-    'Monday': {
-      PhonePortrait: Monday_PhonePortrait
     },
     '1': {
       PhonePortrait: 1 _PhonePortrait
--- client/src/mocks/index.ts
+++ client/src/mocks/index.ts
@@ -1,4 +1,3 @@
 export * from './1/index';
 export * from './Blank/index';
-export * from './Monday/index';
 export * from './Screen/index';
